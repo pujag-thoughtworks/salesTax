f58577c2de2ca43a917245bcf1869536d65d7bfb
Puja Gupta <pujag@thoughtworks.com>
2017-06-10 18:26:02 +0530

    add methods to calculate taxes

--- src/main/com/tw/puja/Item.java
+++ src/main/com/tw/puja/Item.java
@@ -4,20 +4,21 @@
 
 package com.tw.puja;
 
-import java.util.ArrayList;
 
 public class Item {
     public static final String IMPORTED="imported ";
     public static final String[] taxExemptedGoods={"book","chocolate","pill"};
+    public static final int BASIC_TAX_RATE=10;
+    public static final int IMPORT_TAX_RATE=5;
     private String itemName;
     private double itemPrice;
-    private int quantity;
+    private int noOfCopies;
     private boolean isImported;
 
-    Item(String itemName, double itemPrice, int quantity) {
+    Item(String itemName, double itemPrice,int noOfCopies) {
         this.itemName=itemName;
         this.itemPrice=itemPrice;
-        this.quantity=quantity;
+        this.noOfCopies=noOfCopies;
         isImported=itemName.contains(IMPORTED);
     }
     public boolean isTaxExempted() {
@@ -29,8 +30,21 @@ public class Item {
     }
 
 
-   /* public double computeSalesTax() {
-        double tax;
-        if()
-    } */
+    private double computeSalesTax() {
+        double tax=0;
+        if(!isTaxExempted())
+            tax+=BASIC_TAX_RATE*itemPrice/100;
+        if(isImported)
+            tax+=IMPORT_TAX_RATE*itemPrice/100;
+        return tax;
+    }
+
+    public double getPrice() {
+        return (itemPrice+ computeSalesTax())*noOfCopies;
+    }
+
+    public double getSalesTax() {
+        return noOfCopies*computeSalesTax();
+    }
+
 }
